fce258a5e84a970c1ccadafef579d828ed0f399f
James Clarke <james@jamesdavidclarke.com>
2024-08-27 18:40:55 +0100

    Added tests for client.rs

--- src/api/async/client.rs
+++ src/api/async/client.rs
@@ -1,6 +1,22 @@
-// ./src/api/async/client.rs
 use reqwest::Client as ReqwestClient;
 
+/// A client for interacting with the API asynchronously.
+///
+/// This client allows you to specify a base URL for the API and provides
+/// a pre-configured `reqwest::Client` instance to make asynchronous
+/// HTTP requests.
+///
+/// # Examples
+///
+/// ```
+/// use ollama::prelude::*;
+///
+/// let client = Client::new("http://0.0.0.0:11434");
+/// let base_url = client.base_url();
+/// let reqwest_client = client.client();
+/// ```
+///
+/// The example above creates a new `Client` for making asynchronous API calls.
 #[derive(Clone)]
 pub struct Client {
     base_url: String,
@@ -8,6 +24,19 @@ pub struct Client {
 }
 
 impl Client {
+    /// Creates a new asynchronous API client with the given base URL.
+    ///
+    /// # Arguments
+    ///
+    /// * `base_url` - The base URL of the API.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use ollama::prelude::*;
+    ///
+    /// let client = Client::new("http://0.0.0.0:11434");
+    /// ```
     pub fn new(base_url: &str) -> Self {
         Self {
             base_url: base_url.to_string(),
@@ -15,10 +44,30 @@ impl Client {
         }
     }
 
+    /// Returns the base URL of the API.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use ollama::prelude::*;
+    ///
+    /// let client = Client::new("http://0.0.0.0:11434");
+    /// assert_eq!(client.base_url(), "http://0.0.0.0:11434");
+    /// ```
     pub fn base_url(&self) -> &str {
         &self.base_url
     }
 
+    /// Returns a reference to the `reqwest::Client` used for making requests.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use ollama::prelude::*;
+    ///
+    /// let client = Client::new("http://0.0.0.0:11434");
+    /// let reqwest_client = client.client();
+    /// ```
     pub fn client(&self) -> &ReqwestClient {
         &self.client
     }
--- src/api/sync/client.rs
+++ src/api/sync/client.rs
@@ -1,6 +1,22 @@
-// ./src/api/sync/client.rs
 use reqwest::blocking::Client as ReqwestClient;
 
+/// A client for interacting with the API synchronously.
+///
+/// This client allows you to specify a base URL for the API and provides
+/// a pre-configured `reqwest::blocking::Client` instance to make synchronous
+/// HTTP requests.
+///
+/// # Examples
+///
+/// ```
+/// use ollama::prelude::*;
+///
+/// let client = Client::new("http://0.0.0.0:11434");
+/// let base_url = client.base_url();
+/// let reqwest_client = client.client();
+/// ```
+///
+/// The example above creates a new `Client` for making synchronous API calls.
 #[derive(Clone)]
 pub struct Client {
     base_url: String,
@@ -8,6 +24,19 @@ pub struct Client {
 }
 
 impl Client {
+    /// Creates a new synchronous API client with the given base URL.
+    ///
+    /// # Arguments
+    ///
+    /// * `base_url` - The base URL of the API.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use ollama::prelude::*;
+    ///
+    /// let client = Client::new("http://0.0.0.0:11434");
+    /// ```
     pub fn new(base_url: &str) -> Self {
         Self {
             base_url: base_url.to_string(),
@@ -15,10 +44,30 @@ impl Client {
         }
     }
 
+    /// Returns the base URL of the API.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use ollama::prelude::*;
+    ///
+    /// let client = Client::new("http://0.0.0.0:11434");
+    /// assert_eq!(client.base_url(), "http://0.0.0.0:11434");
+    /// ```
     pub fn base_url(&self) -> &str {
         &self.base_url
     }
 
+    /// Returns a reference to the `reqwest::blocking::Client` used for making requests.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use ollama::prelude::*;
+    ///
+    /// let client = Client::new("http://0.0.0.0:11434");
+    /// let reqwest_client = client.client();
+    /// ```
     pub fn client(&self) -> &ReqwestClient {
         &self.client
     }
